9a55869617a7b2078279c56714ef01a9f58807c1
Juan Muno <jmunoz@syner.info>
2021-04-30 18:44:15 +0000

    Corrected the logic with help of my friend, the Jedi Master Luis Carrasco.

--- main.py
+++ main.py
@@ -1,61 +1,50 @@
 """
 Solving https://projecteuler.net/archives
-
 """
-##import os
-##import sys
-##from os import system, name
+sol = 2  ##Select solution to display
 
-##from alive_progress import alive_bar
-##from progress.bar import ShadyBar
-
-##import re
-
-sol = 2 ##Select solution to display
 
 ##Even Fibonacci numbers
-def Sol_02():
-  lim = 4000001
+def Sol_02(lim):
+	print('Sol 02 started ...')
+	suma = 0
+	n1 = 1
+	n2 = 2
+	m = n1 + n2
+	i = 0
+	##Adding first known evens in Fibonacci sequence
+	while m < lim:  #Aqui es m no i
+		if i == 0:
+			m = 0
+		if i == 1:
+			m = 1
+		else:
+			n2 = n1
+			n1 = m
+			m = n1 + n2
+			##print("Is Even: " + str(m % 2))
+		if ((m % 2 == 0)):
+			suma += m
 
-  n1 = 1
-  n2 = 2
-  m = n1 + n2
-  fibonacciEvens = []
-  i = 1
-  ##Adding first known evens in Fibonacci sequence
-  fibonacciEvens.append(n1)
-  fibonacciEvens.append(m)
-  while i < lim:
+		i += 1
 
-    n1 = n2
-    n2 = m
-    m = n1 + n2
-    ##print("Is Even: " + str(m % 2))
-    if ( (m % 2 == 1) ): fibonacciEvens.append(m)
-    ##print(str(n1))
-    i += 1
-    if( i == 1000000 ): print("hit one million")
-    elif( i == 2000000 ): print("hit two million")
-    elif( i == 3000000 ): print("hit three million")
-    elif( i == 3999999 ): print("hit four million")
+	print(str(suma))
 
-  print(fibonacciEvens)
-  suma = sum(fibonacciEvens)
-  print(str(suma))
 
 ##Multiples of 3 and 5
 def Sol_01():
-  sum = 0
-  for n in range(1000):
-    print(str(n))
-    if ( (n % 3) == 0 ): 
-      sum = sum + n
-      print("Mult 3")
-    elif ( (n % 5) == 0 ): 
-      sum = sum + n
-      print("Mult 5")
+	sum = 0
+	for n in range(1000):
+		print(str(n))
+		if ((n % 3) == 0):
+			sum = sum + n
+			print("Mult 3")
+		elif ((n % 5) == 0):
+			sum = sum + n
+			print("Mult 5")
 
-  print(str(sum))
+	print(str(sum))
 
-if( sol == 2 ): Sol_02()
-if( sol == 1 ): Sol_01()
+
+if (sol == 2): Sol_02(4000001)
+if (sol == 1): Sol_01()
